70034158bc397c94061f802b417476caa0d8269d
Albert Yin <albyin@gmail.com>
2016-07-27 18:40:53 -0400

    isolate component code

--- client/pre-build/home/home.controller.js
+++ client/pre-build/home/home.controller.js
@@ -2,5 +2,5 @@ app.controller('HomeController', function($scope) {
   
 	$scope.msgFromScope = "...And I'm a message from the HomeController scope, just so you know that I work!";
 
-	$scope.testMsg = "data from homeController";
+	$scope.testMsg = "data from somewhere";
 });
--- dfxio/dfxio.js
+++ dfxio/dfxio.js
@@ -1,30 +1,23 @@
 (function (angular) {
   'use strict'
 
-  // function TestComponentController(){
-  //   this.message = 'cmonnn TestComponentController';
-  // }
-
   var TestComponent = {
     bindings: {
       message: '='
     },
-    // controller: TestComponentController,
-    // controllerAs: '$ctrl',
     template: '<h1>TestComponent template {{$ctrl.message}}</h1>'
   };
 
-
   angular
-    .module('dfxio', [])
-    .controller('dfxioController', function ($scope) {
-      $scope.message = "Message from dfxio controller";
+    .module('TestComponentModule', [])
+    .controller('TestComponentController', function ($scope) {
+
     })
-    .directive('dfxioDirective', function () {
-      return {
-        restrict: 'E',
-        templateUrl: './dfxio.html'
-      }
-    })
-    .component('testComponent', TestComponent)
+    .component('testComponent', TestComponent);
+
+  var appName = $('[ng-app]').attr('ng-app');
+  
+  angular
+    .module(appName)
+    .requires.push('TestComponentModule');
 })(angular)
